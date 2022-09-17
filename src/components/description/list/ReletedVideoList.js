cb6927d0f277cb29f9425af82620150e3da179a5
mss38 <mss38k@gmail.com>
2022-09-17 20:39:10 +0600

    frontend design and routing completed

---
+++ src/components/description/list/ReletedVideoList.js
@@ -0,0 +1,9 @@
+import ReletedVideoListItem from "./ReletedVideoListItem";
+export default function ReletedVideoList() {
+  return (
+     <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+        
+      <ReletedVideoListItem />
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import VideoGrid from "../components/grid/VideoGrid";
+import Tags from "../components/tags/Tags";
+import Pagination from "../components/ui/Pagination";
+
+export default function Home() {
+  return (
+    <>
+      <Tags />
+      <VideoGrid />
+      <Pagination />
+    </>
+  );
+}
