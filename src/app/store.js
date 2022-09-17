713bd0c7d27d1c592c8659dfd23601afe4dd831e
mss38 <mss38k@gmail.com>
2022-09-17 23:15:33 +0600

    load tags from server

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import videoReducer from "../features/videos/videosSlice";
+import videosReducer from "../features/videos/videosSlice";
+import tagsReducer from "../features/tags/tagsSlice";
 
 export const store = configureStore({
   reducer: {
-    videos: videoReducer,
+    videos: videosReducer,
+    tags: tagsReducer,
   },
 });
--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,5 +1,5 @@
-export default function Tag() {
-  return <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">react</div>;
+export default function Tag({ title }) {
+  return <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">{title}</div>;
 }
 
 // {
